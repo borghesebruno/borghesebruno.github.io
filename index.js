b3f1aedec0a02c672dd284c470d5660285f3c274
Bruno Borghese <bruno.borghese@prontmed.com>
2016-06-24 16:43:05 -0300

    AngularJS (and VSCode commit test)

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    
-});
+var app = angular.module('gardenWebApp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when("/", {templateUrl: "html/home.html", controller: "PageCtrl"})
+    .when("/about", {templateUrl: "html/about.html", controller: "PageCtrl"})
+}]);
+
+app.controller('PageCtrl', ['$scope', function ($scope) {
+    $scope.blas = [];
+    for(var i = 0; i < 15; i++)
+    {
+        $scope.blas.push({name: "bla"});
+    }
+}]);
